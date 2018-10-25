ad221f3f712c7ba98c4070fd95ea46e6f90ae761
root <root@DESKTOP-7S6CRQL.localdomain>
2018-10-26 00:21:35 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/coroi/weatherapp/WeatherApp.java
@@ -0,0 +1,13 @@
+package com.example.coroi.weatherapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WeatherApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather_app);
+    }
+}
